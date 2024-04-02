6ba5dce5e45a9aec78700d30138cb681260ff541
Caiyelb <144827338+Caiyelb@users.noreply.github.com>
2024-04-02 12:14:29 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+
+$("button").click(function() {
+    var username = $(".myInput").val();
+    $(".vote-result").text(username);
+    
+    
+
+    
+    console.log(username); 
+    
+    
+	
+	    
+    
+    
+    
+});
